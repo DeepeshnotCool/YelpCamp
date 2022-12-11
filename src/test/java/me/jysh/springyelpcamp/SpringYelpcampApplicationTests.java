ba54991da1b4d03050860ea803ee2c68281c90f3
deepeshnotcool <deepeshkhatri018@gmail.com>
2022-12-11 19:51:21 +0530

    Initial Commit

---
+++ src/test/java/me/jysh/springyelpcamp/SpringYelpcampApplicationTests.java
@@ -0,0 +1,16 @@
+package me.jysh.springyelpcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringYelpcampApplicationTests {
+
+  @Test
+  public void main() {
+    SpringYelpcampApplication.main(new String[] {});
+  }
+
+  @Test
+  void contextLoads() {}
+}
